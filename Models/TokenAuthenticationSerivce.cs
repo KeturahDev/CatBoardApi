9ade2366696f6233fba80cd14e71e4b39f5c6818
Keturah Howard <keturah.dev@gmail.com>
2020-03-31 09:08:10 -0700

    create authentication service which extends interface and place into models.
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    Co-authored-by: Dusty McCord <dustymccord@gmail.com>
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>

---
+++ Models/TokenAuthenticationSerivce.cs
@@ -0,0 +1,11 @@
+namespace CatBoardApi.Models
+{
+  public class TokenAuthenticationService : IAuthenticateService
+  {
+    public bool IsAuthenticated(TokenRequest request, out string token)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+    
+}

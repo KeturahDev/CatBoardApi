635fd5ac9a58f69fc888b3b2b8fda52670086464
Keturah Howard <keturah.dev@gmail.com>
2020-03-31 09:06:27 -0700

    remove class extending interface from interface.
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    Co-authored-by: Dusty McCord <dustymccord@gmail.com>
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>

--- Interfaces/IAuthenticateService.cs
+++ Interfaces/IAuthenticateService.cs
@@ -6,11 +6,4 @@ namespace CatBoardApi.Models
       bool IsAuthenticated(TokenRequest request, out string token);
   }
 
-  public class TokenAuthenticationService : IAuthenticateService
-  {
-    public bool IsAuthenticated(TokenRequest request, out string token)
-    {
-      throw new System.NotImplementedException();
-    }
-  }
 }
